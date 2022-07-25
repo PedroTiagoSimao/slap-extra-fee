827854e79c42953442321df561689bb21cad766a
Pedro Simao <pedrosimao@gmail.com>
2022-07-25 13:28:49 +0100

    removed restaurant ACF

--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -14,10 +14,10 @@ function add_bag_fee_menu() {
     }
     if(!$menuExist) {
         add_menu_page( 'Definições', 'SLAP', 'administrator', 'slap-settings', 'pages_bag_fee');
-        $bagFeeSubMenu = add_submenu_page('slap-settings', 'Definições Taxa Saco', 'Definições Taxa Saco', 'administrator', 'settings-bag-fee', 'displayBagFeeettings');
+        $bagFeeSubMenu = add_submenu_page('slap-settings', 'Definições Taxa Saco', 'Definições Taxa Saco', 'administrator', 'settings-bag-fee', 'displayBagFeeSettings');
         add_action( 'load-' . $bagFeeSubMenu, 'load_admin_js' );
     } else {
-        $bagFeeSubMenu = add_submenu_page('slap-settings', 'Definições Taxa Saco', 'Definições Taxa Saco', 'administrator', 'settings-bag-fee', 'displayBagFeeettings');
+        $bagFeeSubMenu = add_submenu_page('slap-settings', 'Definições Taxa Saco', 'Definições Taxa Saco', 'administrator', 'settings-bag-fee', 'displayBagFeeSettings');
         add_action( 'load-' . $bagFeeSubMenu, 'load_admin_js' );
     }
 }
@@ -39,7 +39,7 @@ function pages_bag_fee() {
 <?php
 }
 
-function displayBagFeeettings(){
+function displayBagFeeSettings(){
     ?>
     <form action="options.php" method="post">
         <?php 
@@ -86,15 +86,24 @@ function bag_fee_settings() {
     echo             '<input class="" id="bag_fee_is_active" name="bag_fee_plugin_options[is_active]"  type="checkbox" value="1" ' . $checked . ' />
                 </td>
            </tr>';
-    echo '<tr><th>Cobrar 1 (um) saco por restaurante?</th>
-                <td>';
-                $checked = "";
-                if(isset($options['per_restaurant'])){
-                    if($options['per_restaurant']) {
-                        $checked = "checked";
+    echo '<tr><th>Cobrar por cada</th>
+                <td>
+                    <select class="charge" name="bag_fee_plugin_options[charge_per]" id="bag_fee_charge_per">';
+                    $per_selected = "";
+                    if(isset($options['charge_per'])){
+                        $per_selected = $options['charge_per'];
                     }
-                }
-    echo            '<input class="" id="bag_fee_per_restaurant" name="bag_fee_plugin_options[per_restaurant]"  type="checkbox" value="1" ' . $checked . ' />
+                    echo '<option value="per_order" ' . ($per_selected=='per_order' ? "selected" : "" ) .' >Encomenda</option>
+                        <option value="per_item" ' . ($per_selected=='per_item' ? "selected" : "" ) .' >Produto</option>
+                        <option value="per_category" ' . ($per_selected=='per_category' ? "selected" : "" ) .' >Categoria (pai)</option>
+                        <option value="per_tag" ' . ($per_selected=='per_tag' ? "selected" : "" ) .' >Etiqueta (taxa)</option>
+                    </select>
+                        <ul>
+                            <li><strong>1)</strong> A opção "Encomenda" adiciona 1 saco por cada encomenda.</li>
+                            <li><strong>2)</strong> A opção "Produto" adiciona 1 saco por cada produto no carrinho.<br>A quantidade de cada produto também é considerada.</li>
+                            <li><strong>3)</strong> A opção "Categoria" funciona por categoria "pai".<br>Ou seja, só é tido em conta a categoria principal onde os produtos estão inseridos.</li>
+                            <li><strong>4)</strong> Para usar a opção "Etiqueta" deve adicionar a etiqueta <strong>taxa</strong> ao produto</li>
+                        </ul>
                 </td>
             </tr>';
     echo '<tr><th>Titulo da taxa</th><td><input class="regular-text" id="bg_fee_name" name="bag_fee_plugin_options[name]" type="text" value="' . esc_attr( $options['name'] ) .'" /></td></tr>';
--- includes/class-slap-bag-fee.php
+++ includes/class-slap-bag-fee.php
@@ -7,7 +7,6 @@ class SLAP_Bag_Fee {
         add_action( 'woocommerce_cart_calculate_fees', 'add_bag_fee' );
 
         function add_bag_fee(){
-
             // Set vars to prevent PHP errors on WP AJAX notice
             $amount = 0;
 
@@ -17,7 +16,7 @@ class SLAP_Bag_Fee {
                             ? $feeSettings['is_active']
                             : null;
             
-            $name = isset($feeSettings['name'])
+            $feeTitle = isset($feeSettings['name'])
                             ? $feeSettings['name']
                             : null;
             
@@ -35,74 +34,80 @@ class SLAP_Bag_Fee {
                                     ? $feeSettings['per_restaurant']
                                     : null;
 
-            // Check if is taxable and set class
+            $charge_per = isset($feeSettings['charge_per'])
+                                ? $feeSettings['charge_per']
+                                : null;
+
+            // Tags
+            $tagCount = 0;
+
             if($is_active == 1){
                 global $woocommerce;
                 $items = $woocommerce->cart->get_cart();
-                $arrRestaurants = array();
+                $arrCategories = array();
                 
-                // Loop cart to get restaurants
+                // Loop cart to get categories
                 foreach($items as $item => $values) {
                     $productid = $values['data']->get_id();
                     $variation = wc_get_product($productid);
                     // ****************************************************************************************
-                    // se o produto tiver variações, ie for variable, então precisamos obter o produto parent
-                    // porque o produto variable não tem categorias, vêm vazias
+                    // if product variable, get parent because variable does not have category
                     // ****************************************************************************************
                     if ($variation->get_parent_id())
                     {
                         $productid = $variation->get_parent_id();
                     }
 
-                    // Get restaurant IDs
-                    $restaurant_list_ids = wp_get_post_terms($productid,'product_cat',array('fields'=>'ids'));
+                    // Get category IDs
+                    $category_list_ids = wp_get_post_terms($productid,'product_cat',array('fields'=>'ids'));
 
-                    if($restaurant_list_ids){
-                        foreach ($restaurant_list_ids as $id) {
-                    
+                    if($category_list_ids){
+                        foreach ($category_list_ids as $id) {
                             // Get parent IDs
                             $parentcats = get_ancestors($id, 'product_cat');
                             if($parentcats) {
                                 foreach ($parentcats as $cat) {
-                                
-                                    // Get e_restaurente ACF da categoria parent
-                                    $e_restaurante = get_field( "e_restaurante", 'product_cat_' . $cat);
-                                    if($e_restaurante) {
-                                        foreach ($e_restaurante as $rest) {
-                                            if($rest) {
-        
-                                                // If e_restaurante add to array if not there already
-                                                if(!in_array($cat, $arrRestaurants)) {
-                                                    $arrRestaurants[] = $cat;
-                                                }
-                                            }
-                                        }
+                                        if(!in_array($cat, $arrCategories)) {
+                                        $arrCategories[] = $cat;
                                     }
                                 }
-                            } else {
-                                // Get e_restaurente ACF da categoria se é parent
-                                $e_restaurante = get_field( "e_restaurante", 'product_cat_' . $id);
-                                if($e_restaurante) {
-                                    foreach ($e_restaurante as $rest) {
-                                        if($rest) {
-                                            
-                                            // If e_restaurante add to array if not there already
-                                            if(!in_array($id, $arrRestaurants)) {
-                                                $arrRestaurants[] = $id;
-                                            }
-                                        }
-                                    }
+                            } else {  
+                                if(!in_array($id, $arrCategories)) {
+                                    $arrCategories[] = $id;
                                 }
+                            }
+                        }
+                    }
+
+                    // Get product tags
+                    $tags = wp_get_post_terms( $productid, 'product_tag' );
+                    if ( ! empty( $tags ) && ! is_wp_error( $tags ) ){
+                        foreach ( $tags as $tag ) {
+                            if($tag->name == 'taxa'){
+                                $tagCount++;
                             }
                         }
                     }
                 }
 
-                // Check if charging one bag per restaurant or single bag per order
-                if($per_restaurant) {
-                    $restaurante_bag = count($arrRestaurants);
-                } else {
-                    $restaurante_bag = 1;
+                // Charge per order
+                if($charge_per == 'per_order') {
+                    $bag = 1;
+                }
+
+                // Charge per item
+                if($charge_per == 'per_item') {
+                    $bag = $woocommerce->cart->get_cart_contents_count();
+                }
+
+                // Charge per per_category
+                if($charge_per == 'per_category') {
+                    $bag = count($arrCategories);
+                }
+
+                // Charge per per_tag
+                if($charge_per == 'per_tag') {
+                    $bag = $tagCount;
                 }
 
                 if($is_taxable == 1) {
@@ -118,9 +123,12 @@ class SLAP_Bag_Fee {
                     $is_taxable = false;
                 }
 
+                echo '<br>charge per: ' . $charge_per .'<br>';
+                echo 'bags: ' . $bag;
+
                 // Set WC add_fee
                 if($amount > 0) {
-                    WC()->cart->add_fee( __($name .' x ' . $restaurante_bag, 'woocommerce'), $amount*$restaurante_bag, $is_taxable, $tax_class);
+                    WC()->cart->add_fee( __($feeTitle .' x ' . $bag, 'woocommerce'), $amount*$bag, $is_taxable, $tax_class);
                 }
             }
         }
